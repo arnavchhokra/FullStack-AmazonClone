e45ff998eb3a239003c28a1ea5c1b8317af91f4f
Arnav Chhokra <arnavchhokra@gmail.com>
2022-06-29 21:32:22 +0530

    Checkout

--- Ecom-react/src/Checkout.jsx
+++ Ecom-react/src/Checkout.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import './Checkout.css'
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
     <div className="checkout">
-        <div classNAme = "checkout_left">
-            <img className = "checkout_ad" src ="https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/AUX/ILB_BrightColors_NotApproved._TTW_.jpg"/>
+      <div classNAme="checkout_left">
+        <img
+          className="checkout_ad"
+          src="https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/AUX/ILB_BrightColors_NotApproved._TTW_.jpg"
+        />
         <div>
           <h2 className="checkout_title">Your Shopping Basket</h2>
-          
         </div>
-        </div>
+      </div>
 
-        <div classNAme = "checkout_right">
-          <h2>The Subtota will go here</h2>
-        </div>
+      <div classNAme="checkout_right">
+        <Subtotal />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
